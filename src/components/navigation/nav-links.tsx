971c5721e370daa711d2f8854d09bd8ee43fda4d
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-06-01 15:52:05 +0530

    feat using useRouter for navigation

--- src/components/navigation/nav-links.tsx
+++ src/components/navigation/nav-links.tsx
@@ -1,16 +1,12 @@
+"use client";
+import { navConfig } from "@/config/navigate";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 function NavLinks() {
+  const router = useRouter();
   return (
-    <div className="h-full flex flex-col items-start my-10 p-4 w-full">
-      <div>
-        <Link
-          className="text-3xl  border-b border-black font-bold text-black mb-4"
-          href="/navigation"
-        >
-          Home
-        </Link>
-      </div>
+    <div className="h-full flex flex-col items-start my-2 p-4 w-full">
       <div
         className="p-2 flex  flex-col items-start  border-b border-black gap-2 
       "
@@ -18,14 +14,36 @@ function NavLinks() {
         <h3 className="font-medium text-xl text-black">
           This is link based routing
         </h3>
-        <div>
-          <Link
-            className="bg-orange-800 text-white font-bold text-lg rounded-lg p-1"
-            href="/navigation/page2"
-          >
-            Page 2
-          </Link>
+        <div className="flex flex-col gap-1">
+          {navConfig.map((link) => {
+            return (
+              <Link
+                className="bg-orange-800 text-white font-bold text-lg rounded-lg p-1"
+                href={link.url}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
         </div>
+      </div>
+      <div
+        className="p-2 flex  flex-col items-start  border-b border-black gap-2 
+      "
+      >
+        <h3 className="font-medium text-xl text-black">
+          This is useRouter based routing
+        </h3>
+        {navConfig.map((link) => {
+          return (
+            <button
+              className="bg-orange-800 text-white font-bold text-lg rounded-lg p-1"
+              onClick={() => router.push(link.url)}
+            >
+              {link.name}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
