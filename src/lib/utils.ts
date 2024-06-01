cbec2c141468c96358dafbd718b63af128c0ffcd
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-06-02 00:22:42 +0530

    feat artificially lazy function for dashboard

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,30 @@
+import { billData, lastTransact } from "./data";
 import clsx from "clsx";
 import { twMerge } from "tw-merge";
 
 export default function cn(...input: any[]) {
   return twMerge(clsx(input));
+}
+
+export function fetchLastBills(): Promise<number[]> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(billData);
+    }, 4000);
+  });
+}
+
+export function fetchLastTransaction(): Promise<
+  {
+    name: string;
+    invoice: number;
+    paid: boolean;
+    color: string;
+  }[]
+> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(lastTransact);
+    }, 6000);
+  });
 }
