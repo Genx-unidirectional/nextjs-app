b8780268d28ba9ec1bb255197af01440085b7707
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-06-02 00:21:59 +0530

    feat dummy data for dashboard

---
+++ src/lib/data.ts
@@ -0,0 +1,39 @@
+export const billData = [2000, 3200, 1210, 4444];
+export const lastTransact = [
+  {
+    name: "gex",
+    invoice: 1212,
+    paid: false,
+    color: "green",
+  },
+  {
+    name: "lydia",
+    invoice: 122,
+    paid: true,
+    color: "red",
+  },
+  {
+    name: "guillermo",
+    invoice: 12,
+    paid: false,
+    color: "blue",
+  },
+  {
+    name: "delba",
+    invoice: 302,
+    paid: true,
+    color: "pink",
+  },
+  {
+    name: "ganesh",
+    invoice: 2000,
+    paid: false,
+    color: "purple",
+  },
+  {
+    name: "grok",
+    invoice: 3303,
+    paid: true,
+    color: "yellow",
+  },
+];
